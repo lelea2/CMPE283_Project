76d129e66f078182899e50042452592c78734c52
lelea2 <daomaikhanh1988@yahoo.com>
2015-11-07 15:52:22 -0800

    Initial check in for project

---
+++ src/main/java/com/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        return "hello";
+
+    }
+
+}
